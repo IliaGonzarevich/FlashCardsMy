986a2d16148990761cb27b79979286e028583b9d
IliaGonzarevich <gonzarevichave091203@gmail.com>
2021-10-17 22:56:22 +0300

    Initial commit

---
+++ src/main/java/com/example/flashcards/Controller.java
@@ -0,0 +1,22 @@
+package com.example.flashcards;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button Ilyabtn;
+
+    @FXML
+    private Label Ilyalbl;
+
+
+    @FXML
+    void initialize(){
+        Ilyabtn.setOnMouseClicked(mouseEvent -> {
+            Ilyalbl.setText("Ilya is here!");
+        });
+    }
+}

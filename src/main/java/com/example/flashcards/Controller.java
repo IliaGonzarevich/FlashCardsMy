28893f230236333f4289807ba642eb3d80b53a3a
Vladamba <mvladislav777@gmail.com>
2021-10-18 22:15:09 +0300

    Vlad

--- src/main/java/com/example/flashcards/Controller.java
+++ src/main/java/com/example/flashcards/Controller.java
@@ -28,9 +28,7 @@ public class Controller {
     void initialize(){
         ilyaBtn.setOnMouseClicked(mouseEvent -> ilyaLbl.setText("Ilya is here!"));
 
-
         vladBtn.setOnMouseClicked(mouseEvent -> vladLbl.setText("Vlad is here!"));
-
 
         tonyaBtn.setOnMouseClicked(mouseEvent -> tonyaLbl.setText("Tonya is here!"));
     }

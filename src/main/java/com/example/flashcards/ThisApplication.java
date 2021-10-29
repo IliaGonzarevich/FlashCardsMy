c5ccc5c5e5a769ac88091174b5ce05142659f4c3
Vladamba <mvladislav777@gmail.com>
2021-10-29 12:33:07 +0300

    Vlad

--- src/main/java/com/example/flashcards/ThisApplication.java
+++ src/main/java/com/example/flashcards/ThisApplication.java
@@ -11,10 +11,12 @@ public class ThisApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ThisApplication.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 750, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {launch();}
